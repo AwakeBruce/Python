8c4f50707d02322d7f444bae7229c4cd1d56a54e
AwakeBruce <45253748+AwakeBruce@users.noreply.github.com>
2018-11-29 17:18:22 +0800

    Add files via upload

---
+++ try_except_finally.py
@@ -0,0 +1,15 @@
+file_path = r'C:\Users\Administrator\Desktop\root\dir1\cp3_data_size.c'
+
+# try:
+# 	f = open(file_path)
+# except:
+# 	print("No such file or directory!")
+# finally:
+# 	f.close()
+# 	print("Finally closed!")
+
+
+with open(file_path) as f:
+	pass
+
+#上面注释的与下面的等同
